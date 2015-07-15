1f5136607085c2e3f12ebeebabdf4fc381911f1f
Michael Stokes <mjstokes1986@att.net>
2015-07-15 03:08:54 -0400

    checkpoint 3:08am

--- main.rb
+++ main.rb
@@ -10,3 +10,17 @@ get '/' do
 erb :index
 end
 
+get '/about' do
+erb :about
+end
+
+get '/projects' do
+erb :projects
+end
+
+get '/contact'  do
+erb :contact
+end
+
+
+
--- public/main.js
+++ public/main.js
@@ -1,1 +1,24 @@
 console.log('Linked')
+
+$(document).ready(function(){
+
+
+
+$('a').hover(
+
+	function(){
+	$(this).css('color', "#1192C2")
+},
+	function(){
+		$(this).css('color', 'white')
+	});
+
+
+
+
+
+
+
+
+
+})
