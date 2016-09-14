ca7416112c7a566a0a3c366b6d412779ecf5945b
Michael J. Stokes <mjstokes1986@att.net>
2016-09-14 11:36:54 -0400

    checkpoint 11:36am

---
+++ main.rb
@@ -0,0 +1,33 @@
+
+
+require 'sinatra'
+require 'rubygems'
+require 'bundler/setup'
+
+
+
+
+
+set :views, File.dirname(__FILE__) + '/views'
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+
+
+get '/' do 
+erb :index
+end
+
+
+get '/about' do
+erb :about
+end
+
+get '/portfolio' do
+erb :portfolio
+end
+
+get '/contact'  do
+erb :contact
+end
+
+
